baba1dfd842a657006c3ac42b03266700888cca5
heer1411 <37072625+heer1411@users.noreply.github.com>
2018-08-22 13:02:38 +0900

    dynamic programming started

---
+++ c_and_CPP/Dynamic Programming/Memoization/fibonaci.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int n;
+int *memo;
+
+void init(){
+for(int i=0;i<=n ; i++)
+memo[i] = -1;
+
+}
+int fib(int n)
+{
+if(memo[n] == -1)
+{
+  if(n == 1)memo[n] = 1;
+  else if(n == 2)memo[n] = 1;
+  else memo[n] = fib(n-1) + fib(n-2);
+}
+  return memo[n];
+}
+
+
+int main(){
+  cout << "enter the n: "<<endl;
+  cin >> n;
+  memo = (int *)malloc((n+1) * sizeof(int));
+  init();
+  cout << fib(n) ;
+}
